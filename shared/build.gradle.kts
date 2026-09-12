5e184a170b927181c74dbdcd31324edd668d24e1
Bas van den DIkkenberg <bas@dikkenberg.net>
2026-09-12 12:13:08 +0200

    Compile shared Android target against API 36.1

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -12,7 +12,11 @@ plugins {
 kotlin {
     android {
         namespace = "net.dikkenberg.activiteitenweger.shared"
-        compileSdk = 37
+        compileSdk {
+            version = release(36) {
+                minorApiLevel = 1
+            }
+        }
         minSdk = 26
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_17)
