7fb384bfb2e2bc7f3488680505c419996e662ed5
Bas van den DIkkenberg <bas@dikkenberg.net>
2026-09-12 12:21:17 +0200

    Replace deprecated Compose dependency aliases

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -35,10 +35,10 @@ kotlin {
 
     sourceSets {
         commonMain.dependencies {
-            implementation(compose.runtime)
-            implementation(compose.foundation)
-            implementation(compose.material3)
-            implementation(compose.components.resources)
+            implementation(libs.compose.runtime)
+            implementation(libs.compose.foundation)
+            implementation(libs.compose.material3)
+            implementation(libs.compose.components.resources)
 
             implementation(libs.ktor.client.core)
             implementation(libs.ktor.client.content.negotiation)
