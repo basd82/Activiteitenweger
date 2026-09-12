cce28167c2632094fa99e67306dbc84ec800e308
Bas van den DIkkenberg <bas@dikkenberg.net>
2026-09-12 13:52:54 +0200

    Use Android compile SDK 36 for IntelliJ clean sync

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -5,11 +5,7 @@ plugins {
 
 android {
     namespace = "net.dikkenberg.activiteitenweger"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "net.dikkenberg.activiteitenweger"
