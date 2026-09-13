865e6e41e1e7da5948a7c2eabf76436e3f110507
Bas van den DIkkenberg <bas@dikkenberg.net>
2026-09-13 13:54:33 +0200

    Use shared Android file dialog initialization

--- androidApp/src/main/kotlin/net/dikkenberg/activiteitenweger/MainActivity.kt
+++ androidApp/src/main/kotlin/net/dikkenberg/activiteitenweger/MainActivity.kt
@@ -6,15 +6,14 @@ package net.dikkenberg.activiteitenweger
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import io.github.vinceglb.filekit.FileKit
-import io.github.vinceglb.filekit.dialogs.init
+import net.dikkenberg.activiteitenweger.platform.initAndroidFileDialogs
 import net.dikkenberg.activiteitenweger.platform.initAndroidPlatform
 import net.dikkenberg.activiteitenweger.ui.ActiviteitenwegerApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        FileKit.init(this)
+        initAndroidFileDialogs(this)
         initAndroidPlatform(applicationContext)
         setContent {
             ActiviteitenwegerApp()
