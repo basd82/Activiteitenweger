655a1c584afb318f12cc91edef65be009894ce7e
Bas van den DIkkenberg <bas@dikkenberg.net>
2026-09-13 15:25:26 +0200

    Bump Android build for history day selection

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "net.dikkenberg.activiteitenweger"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
+        versionCode = 4
         versionName = "0.1.0"
     }
 
