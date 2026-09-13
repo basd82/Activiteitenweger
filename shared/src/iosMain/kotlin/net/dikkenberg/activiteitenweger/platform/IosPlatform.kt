7e1ba48380bc6f37e05a42c4e8062aeb84c08e71
Bas van den DIkkenberg <bas@dikkenberg.net>
2026-09-13 09:53:29 +0200

    Read iOS app version and build metadata

--- shared/src/iosMain/kotlin/net/dikkenberg/activiteitenweger/platform/IosPlatform.kt
+++ shared/src/iosMain/kotlin/net/dikkenberg/activiteitenweger/platform/IosPlatform.kt
@@ -2,5 +2,14 @@ package net.dikkenberg.activiteitenweger.platform
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.darwin.Darwin
+import platform.Foundation.NSBundle
 
 actual fun createPlatformHttpClient(): HttpClient = HttpClient(Darwin)
+
+actual fun appVersionName(): String =
+    NSBundle.mainBundle.objectForInfoDictionaryKey("CFBundleShortVersionString") as? String
+        ?: "Onbekend"
+
+actual fun appBuildNumber(): String =
+    NSBundle.mainBundle.objectForInfoDictionaryKey("CFBundleVersion") as? String
+        ?: "Onbekend"
