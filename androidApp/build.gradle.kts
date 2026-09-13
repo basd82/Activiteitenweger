b4073ddeb9e8002c532539e597cf319c92494d44
Bas van den DIkkenberg <bas@dikkenberg.net>
2026-09-13 18:27:54 +0200

    Bump Android app to 0.2.1

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "net.dikkenberg.activiteitenweger"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "0.2.0"
+        versionCode = 11
+        versionName = "0.2.1"
     }
 
     compileOptions {
