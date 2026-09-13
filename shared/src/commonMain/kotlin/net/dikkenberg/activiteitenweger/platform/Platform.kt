1fc6e3575bf826dd6d3be93e610e3d17ec96ade7
Bas van den DIkkenberg <bas@dikkenberg.net>
2026-09-13 09:53:22 +0200

    Expose platform app version and build number

--- shared/src/commonMain/kotlin/net/dikkenberg/activiteitenweger/platform/Platform.kt
+++ shared/src/commonMain/kotlin/net/dikkenberg/activiteitenweger/platform/Platform.kt
@@ -3,3 +3,5 @@ package net.dikkenberg.activiteitenweger.platform
 import io.ktor.client.HttpClient
 
 expect fun createPlatformHttpClient(): HttpClient
+expect fun appVersionName(): String
+expect fun appBuildNumber(): String
