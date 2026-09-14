c0ca9e765659217bfb18485bf7cb54a51141ad50
Bas van den DIkkenberg <bas@dikkenberg.net>
2026-09-14 14:51:39 +0200

    Use biometric library in Android shared target

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -61,6 +61,7 @@ kotlin {
         androidMain.dependencies {
             implementation(libs.ktor.client.okhttp)
             implementation(libs.androidx.activity.compose)
+            implementation(libs.androidx.biometric)
             implementation(libs.filekit.dialogs)
         }
 
