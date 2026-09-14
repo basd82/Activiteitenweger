7272614123fd7916eea54e3d7b4caf9ca0e78f9f
Bas van den DIkkenberg <bas@dikkenberg.net>
2026-09-14 15:00:08 +0200

    Bump Android to 0.3.3 build 16

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "net.dikkenberg.activiteitenweger"
         minSdk = 26
         targetSdk = 36
-        versionCode = 15
-        versionName = "0.3.2"
+        versionCode = 16
+        versionName = "0.3.3"
     }
 
     compileOptions {
